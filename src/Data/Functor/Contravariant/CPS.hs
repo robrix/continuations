c3370d17999913eb2bf75a1dc61123b209c42c60
Rob Rix <rob.rix@me.com>
2021-08-13 17:50:27 -0400

    Define a contravariant CPS functor class.

--- src/Data/Functor/Contravariant/CPS.hs
+++ src/Data/Functor/Contravariant/CPS.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Data.Functor.Contravariant.CPS
-() where
+( -- * Contravariant continuation-passing style
+  ContravariantCPS(..)
+, Contravariant(..)
+) where
+
+import Data.Functor.Contravariant
+import Data.Profunctor.Fun
+
+-- Contravariant continuation-passing style
+
+class Contravariant k => ContravariantCPS r k | k -> r where
+  (<#>) :: (a' ~~r~> a) -> (k a -> k a')
+
+  infixl 4 <#>
