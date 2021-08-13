11fd7fb1e72dead6ce6eb96bd347848eb283080a
Rob Rix <rob.rix@me.com>
2021-08-13 15:45:30 -0400

    Define mixfix operator syntax.

--- src/Data/Function/CPS.hs
+++ src/Data/Function/CPS.hs
@@ -1,6 +1,9 @@
 module Data.Function.CPS
 ( -- * CPS functions
   Fun(..)
+  -- ** Mixfix syntax
+, type (~~)
+, type (~>)
 ) where
 
 import Data.Functor.Continuation
@@ -8,3 +11,12 @@ import Data.Functor.Continuation
 -- CPS functions
 
 newtype Fun r a b = Fun { getFun :: r ! b -> r ! a }
+
+
+-- Mixfix syntax
+
+type a ~~r = Fun r a
+type r~> b = r b
+
+infixr 1 ~~
+infixr 0 ~>
