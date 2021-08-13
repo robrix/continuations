5531c372947aeda54d4b8a7836f3b2779654601a
Rob Rix <rob.rix@me.com>
2021-08-13 15:38:09 -0400

    Define a Contravariant instance for !.

--- src/Data/Functor/Continuation.hs
+++ src/Data/Functor/Continuation.hs
@@ -3,8 +3,13 @@ module Data.Functor.Continuation
   type (!)(..)
 ) where
 
+import Data.Functor.Contravariant
+
 -- Continuations
 
 newtype r ! a = K { (!) :: a -> r }
 
 infixl 8 !
+
+instance Contravariant ((!) r) where
+  contramap f (K k) = K (k . f)
