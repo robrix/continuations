8abf98dd6b5433c65551cf427464b2e316bd88e7
Rob Rix <rob.rix@me.com>
2021-08-13 20:33:27 -0400

    Define a conjunction abstraction.

--- src/Data/Bifunctor/Conjunction.hs
+++ src/Data/Bifunctor/Conjunction.hs
@@ -1,16 +1,33 @@
 module Data.Bifunctor.Conjunction
-( exlK
-, exrK
+( -- * Conjunctions
+  Conj(..)
+, exlF
+, exrF
 , deMorganPair
 ) where
 
+import Control.Applicative (liftA2)
 import Data.Functor.Continuation
 
-exlK :: Contravariant k => k a -> k (a, b)
-exlK = contramap fst
+-- Conjunctions
 
-exrK :: Contravariant k => k b -> k (a, b)
-exrK = contramap snd
+class Conj c where
+  (>!!<) :: Applicative f => f a -> f b -> f (a `c` b)
+  infixr 3 >!!<
+  exl :: Contravariant k => k a -> k (a `c` b)
+  exr :: Contravariant k => k b -> k (a `c` b)
+
+instance Conj (,) where
+  (>!!<) = liftA2 (,)
+  exl = contramap fst
+  exr = contramap snd
+
+
+exlF :: (Functor f, Conj c) => f (a `c` b) -> f a
+exlF = fmap (exl (K id) !)
+
+exrF :: (Functor f, Conj c) => f (a `c` b) -> f b
+exrF = fmap (exr (K id) !)
 
 
 deMorganPair :: Contravariant k => Either (k a) (k b) -> k (a, b)
