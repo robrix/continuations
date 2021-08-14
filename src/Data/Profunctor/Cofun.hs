361e0903c5751bfb48f8eb480be9ef30cd29367d
Rob Rix <rob.rix@me.com>
2021-08-13 20:38:14 -0400

    Generalize coap to Disj.

--- src/Data/Profunctor/Cofun.hs
+++ src/Data/Profunctor/Cofun.hs
@@ -45,5 +45,5 @@ cocurry f = Fun (\ k -> K (\ (b :>- c) -> (k <!!> b) ! f c))
 uncocurry :: Fun r (Cofun r b c) a -> Fun r c (Either a b)
 uncocurry f = Fun (\ k -> K (\ c -> f # inlK k ! (inrK k >- c)))
 
-coap :: Fun r c (Either (Cofun r b c) b)
+coap :: Disj d => Fun r c (Cofun r b c `d` b)
 coap = Fun (\ k -> (inrK k >-) >$< inlK k)
