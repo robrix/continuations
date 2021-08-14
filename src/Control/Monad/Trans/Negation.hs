dec13fe7a1bf09ecfb23df51dcd19bcfd6d61734
Rob Rix <rob.rix@me.com>
2021-08-14 08:23:42 -0400

    Rename !! to Neg.

--- src/Control/Monad/Trans/Negation.hs
+++ src/Control/Monad/Trans/Negation.hs
@@ -1,19 +1,19 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Control.Monad.Trans.Negation
 ( -- * Continuation monad
-  type (!!)(..)
+  Neg(..)
 ) where
 
 import Control.Applicative (liftA2)
 import Data.Functor.Continuation
 
-newtype k !! a = Neg { runNeg :: k (k a) }
+newtype Neg k a = Neg { runNeg :: k (k a) }
 
-instance Contravariant k => Functor ((!!) k) where
+instance Contravariant k => Functor (Neg k) where
   fmap f (Neg k) = Neg (contramap (contramap f) k)
   a <$ Neg k = Neg (contramap (a >$) k)
 
-instance Continuation r k => Applicative ((!!) k) where
+instance Continuation r k => Applicative (Neg k) where
   pure = Neg . in2K
   liftA2 f (Neg a) (Neg b) = Neg (inK (\ c -> a ! inK (\ a -> b ! inK (\ b -> c ! f a b))))
 
