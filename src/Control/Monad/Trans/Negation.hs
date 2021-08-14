cdaa98c773b1c947ff8e444c391837c8043d3161
Rob Rix <rob.rix@me.com>
2021-08-14 13:52:45 -0400

    Rename getNeg to getNegT.

--- src/Control/Monad/Trans/Negation.hs
+++ src/Control/Monad/Trans/Negation.hs
@@ -15,7 +15,7 @@ module Control.Monad.Trans.Negation
 import Control.Applicative (liftA2)
 import Data.Functor.Continuation
 
-newtype NegT k m a = NegT { getNeg :: k (k a) }
+newtype NegT k m a = NegT { getNegT :: k (k a) }
 
 instance Contravariant k => Functor (NegT k m) where
   fmap f (NegT k) = NegT (contramap (contramap f) k)
@@ -26,7 +26,7 @@ instance Continuation r k => Applicative (NegT k m) where
   liftA2 f = neg2 (\ a b c -> a (b . (c .) . f))
 
 instance Continuation r k => Monad (NegT k m) where
-  m >>= f = NegT (adjunct @_ @k (adjunct getNeg . adjunct (getNeg . f)) m)
+  m >>= f = NegT (adjunct @_ @k (adjunct getNegT . adjunct (getNegT . f)) m)
 
 
 -- Construction
@@ -35,13 +35,13 @@ neg :: Continuation r k => ((a -> r) -> r) -> NegT k m a
 neg = NegT . in2K
 
 neg1 :: Continuation r k => (((a -> r) -> r) -> ((b -> r) -> r)) -> (NegT k m a -> NegT k m b)
-neg1 f = NegT . in2K . f . ex2K . getNeg
+neg1 f = NegT . in2K . f . ex2K . getNegT
 
 neg2 :: Continuation r k => (((a -> r) -> r) -> ((b -> r) -> r) -> ((c -> r) -> r)) -> (NegT k m a -> NegT k m b -> NegT k m c)
-neg2 f a b = NegT (in2K (ex2K (getNeg a) `f` ex2K (getNeg b)))
+neg2 f a b = NegT (in2K (ex2K (getNegT a) `f` ex2K (getNegT b)))
 
 
 -- Elimination
 
 runNeg :: Continuation r k => k a -> k (NegT k m a)
-runNeg = adjunct getNeg
+runNeg = adjunct getNegT
