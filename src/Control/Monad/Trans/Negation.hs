26b2867760882406111b8ff407a606933d0c3946
Rob Rix <rob.rix@me.com>
2021-08-14 13:54:39 -0400

    Parameterize everything by the monad.

--- src/Control/Monad/Trans/Negation.hs
+++ src/Control/Monad/Trans/Negation.hs
@@ -21,27 +21,27 @@ instance Contravariant k => Functor (NegT k m) where
   fmap f (NegT k) = NegT (contramap (contramap f) k)
   a <$ NegT k = NegT (contramap (a >$) k)
 
-instance Continuation r k => Applicative (NegT k m) where
+instance Continuation (m r) k => Applicative (NegT k m) where
   pure = NegT . unit
   liftA2 f = neg2 (\ a b c -> a (b . (c .) . f))
 
-instance Continuation r k => Monad (NegT k m) where
+instance Continuation (m r) k => Monad (NegT k m) where
   m >>= f = NegT (adjunct @_ @k (adjunct getNegT . adjunct (getNegT . f)) m)
 
 
 -- Construction
 
-neg :: Continuation r k => ((a -> r) -> r) -> NegT k m a
+neg :: Continuation (m r) k => ((a -> m r) -> m r) -> NegT k m a
 neg = NegT . in2K
 
-neg1 :: Continuation r k => (((a -> r) -> r) -> ((b -> r) -> r)) -> (NegT k m a -> NegT k m b)
+neg1 :: Continuation (m r) k => (((a -> m r) -> m r) -> ((b -> m r) -> m r)) -> (NegT k m a -> NegT k m b)
 neg1 f = NegT . in2K . f . ex2K . getNegT
 
-neg2 :: Continuation r k => (((a -> r) -> r) -> ((b -> r) -> r) -> ((c -> r) -> r)) -> (NegT k m a -> NegT k m b -> NegT k m c)
+neg2 :: Continuation (m r) k => (((a -> m r) -> m r) -> ((b -> m r) -> m r) -> ((c -> m r) -> m r)) -> (NegT k m a -> NegT k m b -> NegT k m c)
 neg2 f a b = NegT (in2K (ex2K (getNegT a) `f` ex2K (getNegT b)))
 
 
 -- Elimination
 
-runNeg :: Continuation r k => k a -> k (NegT k m a)
+runNeg :: Continuation (m r) k => k a -> k (NegT k m a)
 runNeg = adjunct getNegT
