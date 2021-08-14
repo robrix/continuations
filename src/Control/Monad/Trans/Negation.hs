eddc3634a50b70e5301fee3338cb6e7bc965d12e
Rob Rix <rob.rix@me.com>
2021-08-14 11:56:05 -0400

    Give Neg a poly-kinded parameter for the underlying monad.

--- src/Control/Monad/Trans/Negation.hs
+++ src/Control/Monad/Trans/Negation.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UndecidableInstances #-}
 module Control.Monad.Trans.Negation
@@ -14,33 +15,33 @@ module Control.Monad.Trans.Negation
 import Control.Applicative (liftA2)
 import Data.Functor.Continuation
 
-newtype Neg k a = Neg { getNeg :: k (k a) }
+newtype Neg k m a = Neg { getNeg :: k (k a) }
 
-instance Contravariant k => Functor (Neg k) where
+instance Contravariant k => Functor (Neg k m) where
   fmap f (Neg k) = Neg (contramap (contramap f) k)
   a <$ Neg k = Neg (contramap (a >$) k)
 
-instance Continuation r k => Applicative (Neg k) where
+instance Continuation r k => Applicative (Neg k m) where
   pure = Neg . unit
   liftA2 f = neg2 (\ a b c -> a (b . (c .) . f))
 
-instance Continuation r k => Monad (Neg k) where
+instance Continuation r k => Monad (Neg k m) where
   m >>= f = Neg (adjunct @_ @k (adjunct getNeg . adjunct (getNeg . f)) m)
 
 
 -- Construction
 
-neg :: Continuation r k => ((a -> r) -> r) -> Neg k a
+neg :: Continuation r k => ((a -> r) -> r) -> Neg k m a
 neg = Neg . in2K
 
-neg1 :: Continuation r k => (((a -> r) -> r) -> ((b -> r) -> r)) -> (Neg k a -> Neg k b)
+neg1 :: Continuation r k => (((a -> r) -> r) -> ((b -> r) -> r)) -> (Neg k m a -> Neg k m b)
 neg1 f = Neg . in2K . f . ex2K . getNeg
 
-neg2 :: Continuation r k => (((a -> r) -> r) -> ((b -> r) -> r) -> ((c -> r) -> r)) -> (Neg k a -> Neg k b -> Neg k c)
+neg2 :: Continuation r k => (((a -> r) -> r) -> ((b -> r) -> r) -> ((c -> r) -> r)) -> (Neg k m a -> Neg k m b -> Neg k m c)
 neg2 f a b = Neg (in2K (ex2K (getNeg a) `f` ex2K (getNeg b)))
 
 
 -- Elimination
 
-runNeg :: Continuation r k => k a -> k (Neg k a)
+runNeg :: Continuation r k => k a -> k (Neg k m a)
 runNeg = adjunct getNeg
