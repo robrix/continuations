9569a2be4af7d24e6dd3f60b23611ee0eda66b64
Rob Rix <rob.rix@me.com>
2021-08-14 08:13:58 -0400

    Define a Functor instance for !!.

--- src/Control/Monad/Trans/Negation.hs
+++ src/Control/Monad/Trans/Negation.hs
@@ -3,4 +3,10 @@ module Control.Monad.Trans.Negation
   type (!!)(..)
 ) where
 
+import Data.Functor.Contravariant
+
 newtype k !! a = Neg { runNeg :: k (k a) }
+
+instance Contravariant k => Functor ((!!) k) where
+  fmap f (Neg k) = Neg ((f >$<) >$< k)
+  a <$ Neg k = Neg ((a >$) >$< k)
