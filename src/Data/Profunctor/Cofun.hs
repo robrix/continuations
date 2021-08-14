94afdab57f48ed36c15b46805b04733958a5f57d
Rob Rix <rob.rix@me.com>
2021-08-13 20:14:40 -0400

    Define a constructor for Cofun.

--- src/Data/Profunctor/Cofun.hs
+++ src/Data/Profunctor/Cofun.hs
@@ -1,6 +1,8 @@
 module Data.Profunctor.Cofun
 ( -- * Co-functions
   Cofun(..)
+  -- * Construction
+, (>-)
 ) where
 
 import Data.Functor.Continuation
@@ -10,3 +12,11 @@ import Data.Functor.Continuation
 data Cofun r b a = (:>-) { coreturn :: r ! b, coconst :: a }
 
 infixr 0 :>-
+
+
+-- Construction
+
+(>-) :: (r ! b) -> a -> Cofun r b a
+(>-) = (:>-)
+
+infixr 0 >-
