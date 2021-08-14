e22c1bcbeaac374eceeb4b0f0ad7271f812bfc72
Rob Rix <rob.rix@me.com>
2021-08-13 21:12:26 -0400

    Define a Contrapply class.

--- src/Data/Functor/Contravariant/Apply.hs
+++ src/Data/Functor/Contravariant/Apply.hs
@@ -1,2 +1,23 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Data.Functor.Contravariant.Apply
-() where
+( -- * Contravariant applicative functors
+  Contrapply(..)
+) where
+
+import Data.Functor.Continuation
+import Data.Functor.Contravariant.CPS
+import Data.Profunctor.Cofun
+import Data.Profunctor.Fun
+
+-- Contravariant applicative functors
+
+class ContravariantCPS r k => Contrapply r k | k -> r where
+  {-# MINIMAL coliftC2 | (<&>) #-}
+
+  coliftC2 :: ((b >-r-~ c) ~~r~> a) -> k a -> k b -> k c
+  coliftC2 f a b = f <#> a <&> b
+
+  (<&>) :: k (a >-r-~ b) -> k a -> k b
+  (<&>) = coliftC2 (fun (!))
+
+  infixl 3 <&>
