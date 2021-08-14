ae96f0f37757e66d2631bf849264593d6e3c6a6f
Rob Rix <rob.rix@me.com>
2021-08-14 13:35:11 -0400

    Define a ContravariantCPS instance for Flip.

--- src/Data/Functor/Contravariant/Flip.hs
+++ src/Data/Functor/Contravariant/Flip.hs
@@ -1,13 +1,20 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE UndecidableInstances #-}
 -- | 'Contravariant' functors from 'Profunctor's.
 --
 -- Generalizes "Data.Functor.Contravariant".'Op' to any 'Profunctor'.
 module Data.Functor.Contravariant.Flip
 ( -- * Flipped profunctors
   Flip(..)
+  -- * CPS-traversable profunctors
+, ProfunctorCPS(..)
 ) where
 
-import Data.Functor.Contravariant
+import Control.Arrow
+import Data.Functor.Continuation
+import Data.Functor.Contravariant.CPS
 import Data.Profunctor
+import Data.Profunctor.Fun
 
 -- Flipped profunctors.
 
@@ -15,3 +22,21 @@ newtype Flip p a b = Flip { getFlip :: p b a }
 
 instance Profunctor p => Contravariant (Flip p a) where
   contramap f = Flip . lmap f . getFlip
+
+instance ProfunctorCPS r p => ContravariantCPS r (Flip p a) where
+  f <#> a = Flip (lmapCPS f (getFlip a))
+
+
+-- CPS-traversable profunctors
+
+class Profunctor p => ProfunctorCPS r p | p -> r where
+  {-# MINIMAL dimapCPS | (lmapCPS, rmapCPS) #-}
+
+  dimapCPS :: (a' ~~r~> a) -> (b ~~r~> b') -> p a b -> p a' b'
+  dimapCPS f g = lmapCPS f . rmapCPS g
+
+  lmapCPS :: (a' ~~r~> a) -> p a b -> p a' b
+  lmapCPS = (`dimapCPS` arr id)
+
+  rmapCPS :: (b ~~r~> b') -> p a b -> p a b'
+  rmapCPS = (arr id `dimapCPS`)
