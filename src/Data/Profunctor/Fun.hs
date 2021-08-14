350b361af5af501ac9747837abcfc7d7b0666545
Rob Rix <rob.rix@me.com>
2021-08-13 20:48:10 -0400

    Define an eliminator for Fun.

--- src/Data/Profunctor/Fun.hs
+++ src/Data/Profunctor/Fun.hs
@@ -6,6 +6,15 @@ module Data.Profunctor.Fun
 , type (~>)
   -- ** Construction
 , fun
+  -- ** Elimination
+, elimFun
 ) where
 
+import Data.Functor.Continuation
+import Data.Profunctor.Cofun
 import Data.Profunctor.Fun.Internal
+
+-- Elimination
+
+elimFun :: (b >-r-~ a) -> r ! (a ~~r~> b)
+elimFun (b :>- a) = K (\ f -> f # b ! a)
